cbbbede1c14756e342cec59efb9a372a7feb78e4
pinky_kraken <pinkypsy@gmail.com>
2020-11-16 21:05:30 +0200

    013 vid

--- newScript.js
+++ newScript.js
@@ -1,4 +1,6 @@
-const numberOfFilms = +prompt("Сколько фильмов?", "");
+'use strict';
+
+const numberOfFilms = prompt("Сколько фильмов?", "");
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -8,11 +10,28 @@ const personalMovieDB = {
     private: false
 };
 
-const lastWatched = prompt("Last watched?", ""),
-      score = prompt("Score?", ""),
-      a = prompt("Last watched?", ""),
-      b = prompt("Score?", "");
 
-personalMovieDB.movies[lastWatched] = score;
+for (let i = 0; i < 2; i++) {
+    const lastWatchedFilm = prompt("Last watched?", ""),
+        score = +prompt("Score?", "");
 
+        if (lastWatchedFilm != null && score != null && lastWatchedFilm != "" && score != "" && lastWatchedFilm.length < 50){
+            personalMovieDB.movies[lastWatchedFilm] = score;
+            console.log("done");
+        } else {
+            console.log("error");
+            i--;
+        }
 
+        if (personalMovieDB.count < 10){
+            console.log("Мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+            console.log("Классический зритель");
+        } else if (personalMovieDB.count >= 30){
+            console.log("Киноман");
+        } else {
+            console.log("Error")
+        }
+    }
+
+console.log(personalMovieDB);
